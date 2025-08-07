0341a23a8b384b58dc063974e34cce9a14ccef76
Rafael Marques <rafaeldecarvalhomarques@gmail.com>
2025-08-06 22:13:31 -0300

    add `type` to  request and response params  on status function

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,6 +1,9 @@
 import database from "../../../../infra/database"
+import { NextApiRequest, NextApiResponse } from "next";
 
-async function status(request, response) {
+
+async function status( _request: NextApiRequest , response: NextApiResponse) {
+
   const result = await database.query('SELECT 1 + 1;')
   console.log(result.rows)
   response.status(200).json({ chave: "são acima da média." });
