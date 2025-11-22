204389d596996fe53f2c079c8bfec2a32e2cd177
Rafael Marques <rafaeldecarvalhomarques@gmail.com>
2025-11-22 19:17:38 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,12 +1,38 @@
-import database from "infra/database"
+import database from "infra/database";
 import { NextApiRequest, NextApiResponse } from "next";
 
+async function status(request: NextApiRequest, response: NextApiResponse) {
+  const updatedAt = new Date().toISOString();
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionVelue = databaseVersionResult.rows[0]?.server_version;
 
-async function status( _request: NextApiRequest , response: NextApiResponse) {
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0]?.max_connections;
 
-  const result = await database.query('SELECT 1 + 1;')
-  console.log(result.rows)
-  response.status(200).json({ chave: "são acima da média." });
+  const databaseName = process.env.POSTGRES_DB;
+  
+    const databaseOpenedConnectionsResult = await database.query({
+      text: `SELECT  count(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+      values: [databaseName]
+    }
+  );
+
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+  response.status(200).json({
+    updated_at: updatedAt,
+    depenencies: {
+      database: {
+        version: databaseVersionVelue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.ts
+++ tests/integration/api/v1/status/get.test.ts
@@ -1,4 +1,16 @@
 test('GET to api/v1/status should return 200', async() => {
   const response = await fetch('http://localhost:3000/api/v1/status');
+  const responseBody = await response.json();
+  console.log(responseBody);
+
   expect(response.status).toBe(200);
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(parsedUpdatedAt).toEqual(responseBody.updated_at);
+
+  expect(responseBody.depenencies.database.version).toEqual('16.0');
+  expect(responseBody.depenencies.database.max_connections).toEqual(100);
+  expect(responseBody.depenencies.database.opened_connections).toEqual(1);
+
 })
+
