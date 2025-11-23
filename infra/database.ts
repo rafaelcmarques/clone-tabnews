44d96ae9d3568c8e640faebcc9260e25142deafc
Rafael Marques <rafaeldecarvalhomarques@gmail.com>
2025-11-23 16:15:42 -0300

    makes database.js `client.connect()` more robust to error

--- infra/database.ts
+++ infra/database.ts
@@ -1,27 +1,34 @@
 import { Client, QueryConfig, QueryResult } from "pg";
 
-async function query (queryObject: string | QueryConfig): Promise<QueryResult> {
+async function query(queryObject: string | QueryConfig): Promise<QueryResult> {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: Number(process.env.POSTGRES_PORT),
-    user: process.env.POSTGRES_USER ,
+    user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-  })
-  await client.connect()
+  });
+
+  console.log("Credenciais do banco:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
 
   try {
-      const result = await client.query(queryObject)
-      return result; 
+    await client.connect();
+    const result = await client.query(queryObject);
+    return result;
   } catch (error) {
-    console.error('Database query error:', error);
+    console.error("Database query error:", error);
     throw error;
-  }finally {
-      await client.end()
+  } finally {
+    await client.end();
   }
-  
 }
 
 export default {
-  query
-}
+  query,
+};
