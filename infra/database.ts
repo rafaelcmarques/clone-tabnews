9f3aa367fd7a4d5831a4d8532f8746a4eb8e1139
Rafael Marques <rafaeldecarvalhomarques@gmail.com>
2025-11-23 17:28:20 -0300

    handles database ssl connection

--- infra/database.ts
+++ infra/database.ts
@@ -7,6 +7,7 @@ async function query(queryObject: string | QueryConfig): Promise<QueryResult> {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === 'development' ? false  : true
   });
 
   console.log("Credenciais do banco:", {
